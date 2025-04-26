2b6abdfb7418c08c6204708721ba4c3cfb46af6b
AnandaCahya <tgigroupjap@gmail.com>
2025-04-26 22:26:37 +0700

    fix [style] Fix some styling issue

--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -9,20 +9,20 @@ interface SettingMenuProps {
 
 const SettingMenu = ({ icon, name }: SettingMenuProps) => {
   return (
-    <div
+    <button
       className={`relative bg-[#404040] group overflow-hidden flex flex-row gap-4 p-4 items-center z-20
-        before:absolute before:pointer-events-none before:z-10 before:left-0 before:top-0
+        before:absolute before:z-10 before:left-0 before:top-0
         before:min-h-full before:rounded-r-full before:transition-all before:duration-500
-        hover:shadow hover:shadow-[#C667F7]
-        before:w-0 hover:before:w-screen before:bg-[#C667F7]`}
+        active:shadow active:shadow-[#C667F7]
+        before:w-0 active:before:w-screen before:bg-[#C667F7]`}
     >
-      <span className="z-20 transition-all duration-350 group-hover:font-semibold group-hover:text-[#101010]">
+      <span className="z-20 transition-all duration-350 group-active:font-semibold group-active:text-[#101010]">
         {icon}
       </span>
-      <span className="z-20 transition-all duration-350 group-hover:font-medium group-hover:text-[#101010]">
+      <span className="z-20 transition-all duration-350 group-active:font-medium group-active:text-[#101010]">
         {name}
       </span>
-    </div>
+    </button>
   );
 };
 
