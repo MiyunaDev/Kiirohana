2112b1467017cc458d6830246505b9a7d7b615e7
AnandaCahya <tgigroupjap@gmail.com>
2026-05-11 20:13:58 +0700

    feat [notfound, routes] Add notfound page and clean routes

--- src/main.tsx
+++ src/main.tsx
@@ -13,33 +13,10 @@ import AppLayout from './layout/AppLayout.tsx';
 import NavigativeLayout from './layout/NavigativeLayout.tsx';
 import Library from './pages/Library/Library.tsx';
 import History from './pages/History/History.tsx';
-import Detail from './pages/Detail/Detail.tsx';
-import DetailLayout from './layout/DetailLayout.tsx';
-// import NovelReader from './pages/Shinobu/pages/Novel/Reader.tsx';
 
 import Settings from './pages/Settings/Settings.tsx';
 import ServicesSetting from './pages/Settings/ServicesSetting.tsx';
-
-// ------------------- Shinobu
-import Latest from "./Shinobu/pages/Browse/Latest.tsx"
-// import ShinobuChat from "./pages/Shinobu/Chat/ChatPage.tsx"
-import ComicReader from './Shinobu/pages/Comic/Reader.tsx';
-import UserProfilePage from './Shinobu/pages/Detail/User.tsx';
-import InstalledShinobu from './Shinobu/pages/System/Main.tsx';
-import ShinobuBootstrap from './Shinobu/pages/System/Bootsrap.tsx';
-import ShinobuLogin from './Shinobu/pages/System/Login.tsx';
-import ShinobuDetail from './Shinobu/pages/Detail/Media.tsx';
-import MainLayout from './Shinobu/layout/Main.layout.tsx';
-import Landing from './Shinobu/pages/Landing/Landing.tsx';
-import SearchLayout from './Shinobu/layout/Search.layout.tsx';
-import Advance from './Shinobu/pages/Browse/Advance.tsx';
-import HistoryPage from './Shinobu/pages/History/History.tsx';
-import AccountCenterPage from './Shinobu/pages/System/AccountCenter.tsx';
-import LibraryPage from './Shinobu/pages/Library/Library.tsx';
-import CollectionDetailPage from './Shinobu/pages/Library/CollectionDetailPage.tsx';
-import Player from './Shinobu/pages/Player/Player.tsx';
-import DeveloperApplicationsPage from './Shinobu/pages/Developer/DeveloperPortal.tsx';
-import { ShinobuProvider } from './Shinobu/contexts/ShinobuContext.tsx';
+import NotFound from './pages/Error/NotFound.tsx';
 
 const router = createHashRouter([
   {
@@ -65,72 +42,11 @@ const router = createHashRouter([
           { path: "history", Component: History }
         ]
       },
-      {
-        path: "detail", Component: DetailLayout, children: [
-          { index: true, Component: Detail },
-          {
-            path: "reader", children: [
-              // { path: "comic", Component: ComicReader },
-              // { path: "novel", Component: NovelReader },
-            ]
-          }
-        ]
-      },
-      {
-        path: "shinobu", children: [
-          { index: true, Component: InstalledShinobu },
-          {
-            path: ":shinobuid", Component: ShinobuProvider, children: [
-              {
-                index: true, Component: ShinobuBootstrap
-              },
-              {
-                path: "login", Component: ShinobuLogin
-              },
-              {
-                path: "app", Component: MainLayout, children: [
-                  { path: "account-center", Component: AccountCenterPage },
-                  { path: "developer-portal", Component: DeveloperApplicationsPage },
-                  { path: "home", Component: Landing },
-                  {
-                    path: "library", children: [
-                      { index: true, Component: LibraryPage },
-                      { path: "collection/:id", Component: CollectionDetailPage }
-                    ]
-                  },
-                  { path: "history", Component: HistoryPage },
-                  {
-                    path: "search", Component: SearchLayout, children: [
-                      { path: "latest", Component: Latest },
-                      { path: "advance", Component: Advance }
-                    ]
-                  }
-                ]
-              },
-              {
-                path: "profile", Component: UserProfilePage
-              },
-              {
-                path: "detail/:mediaId",
-                Component: ShinobuDetail,
-              },
-              // {
-              //   path: "reader/novel/:chapterId",
-              //   Component: NovelReader,
-              // },
-              {
-                path: "reader/comic/:chapterId",
-                Component: ComicReader,
-              },
-              {
-                path: "player/:chapterId",
-                Component: Player,
-              },
-            ]
-          }
-        ]
-      }
     ]
+  },
+  {
+    path: "*",
+    Component: NotFound
   }
 ]);
 
